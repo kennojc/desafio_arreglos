f5573064665a705e7468af2f56c905e00b3364de
Kenneth Johnson C <kennojc@gmail.com>
2020-10-31 12:35:20 -0300

    avanzo el 4to

---
+++ filtro_procesos.rb
@@ -0,0 +1,29 @@
+limit=ARGV[0].to_i
+
+def read_file(filename)
+    original_data = open(filename).readlines 
+    lines = original_data.count
+    array = []
+    lines.times do |i|
+    array << original_data[i].to_i 
+    end
+    return array
+end
+
+array = read_file("procesos.data")
+
+def condition (limit,array)
+    to_file = []
+    n = array.count
+    n.times do |i|
+        to_file << array[i] if array[i]>limit
+    end
+   return to_file 
+end
+
+output = condition(limit,array)
+File.write("procesos_filtrados.data",output.join("\n"))
+
+
+
+
